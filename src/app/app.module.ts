a72be2a604bf1e2557294848420e794e234afae7
mksbnt <p38o68@gmail.com>
2018-12-07 16:01:03 +0200

    generate component my-table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyTableComponent } from './my-table/my-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyTableComponent
   ],
   imports: [
     BrowserModule
