3e0ae3f435132fa497de38632909915bdbcf8fcf
mksbnt <p38o68@gmail.com>
2018-12-07 18:00:16 +0200

    show all products

--- src/app/my-table/my-table.component.ts
+++ src/app/my-table/my-table.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../../product';
+import { PRODUCTS } from '../../productsList';
 
 @Component({
   selector: 'app-my-table',
   templateUrl: './my-table.component.html',
   styleUrls: ['./my-table.component.css']
 })
+
 export class MyTableComponent implements OnInit {
+
+  product: Product = {
+    id: 1,
+    name: "product 1",
+    price: 100
+  };
+
+  products = PRODUCTS;
 
   constructor() { }
 
